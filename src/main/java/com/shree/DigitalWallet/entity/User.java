9088f0a089a689d17d531739d2a8cc3f5ee5a34a
Shree2004 <shreeshinde3007@gmail.com>
2026-03-02 13:34:08 +0530

    Added User entity

---
+++ src/main/java/com/shree/DigitalWallet/entity/User.java
@@ -0,0 +1,28 @@
+package com.shree.DigitalWallet.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false, unique = true)
+    private String emailId;
+
+    @Column(nullable = false, unique = true)
+    private String mobileNumber;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/shree/DigitalWallet/entity/Wallet.java
@@ -0,0 +1,4 @@
+package com.shree.DigitalWallet.entity;
+
+public class Wallet {
+}
